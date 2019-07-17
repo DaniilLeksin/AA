67dbe72d716740f1934e3e38808cf28665481869
Daniil Leksin <danilaleksin@gmail.com>
2019-07-17 21:46:19 +0300

    show + create

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,15 +1,16 @@
-class Feedback < ApplicationController
+class FeedbacksController < ApplicationController
+  protect_from_forgery with: :null_session
   def show
     url = feedback_params[:url]
-    feedback_list = Feedback.find_by(url: url)
+    feedback_list = Feedback.where(url: url)
     render json: { status: 'ok', feedbacks: feedback_list }
   end
 
   def create
     require_params = feedback_params
     count_by_url = Feedback.where(url: require_params[:url]).count
-    if count_by_url > 20
-      Feedback.where(url: require_params[:url]).order(:asc).first.tap do |feedback|
+    if count_by_url > 5
+      Feedback.where(url: require_params[:url]).order(updated_at: :asc).first.tap do |feedback|
         feedback.text = require_params[:text]
         feedback.nickname = require_params[:nickname]
       end.save
